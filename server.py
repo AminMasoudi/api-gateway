b57503cd33797ec348751d99c514c261c2ea5075
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-25 03:24:57 +0330

    IMP => pass

--- server.py
+++ server.py
@@ -3,10 +3,26 @@ from starlette.requests import Request
 from starlette.responses import Response
 from starlette.routing import Route, Host, Router
 import json
+import uvicorn
+
+with open("config.json") as file:
+    conf:dict[str, list[dict[str, str]]] = json.loads(file.read())
+    for k,v in conf.items():
+        conf[k].sort(key=lambda x: x["path"], reverse=True)
+
 async def router(request: Request):
-    path = request.path_params["path"]
+    path: str = "/"+request.path_params["path"]
     host = request.path_params["host"]
-    return Response(f"path: {path} \nHost: {host}")
+    result = conf
+
+    for route in conf.get(host):
+        if path.startswith(route["path"]):
+            result = f"Redirected to {route["service"]}"
+            break
+
+    return Response(f"path: {path} \nHost: {host}\nresult: {result}")
+
+
 routes=[
     Route("/{path:path}", router)
     ]
@@ -15,4 +31,8 @@ routes = [
     Host('{host}', routes, name="site_api")
 ]
 app = Starlette(debug=True, routes=routes)
+
+if __name__ == "__main__":
+    print(conf)
+    uvicorn.run("server:app", host="127.0.0.1", port=8000, log_level="info")
 
