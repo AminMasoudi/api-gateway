517966f8467ba5763495ca0c03efaca3f0bdc562
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-25 02:44:03 +0330

    INIT => ...

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 from starlette.applications import Starlette
 from starlette.requests import Request
 from starlette.responses import Response
-from starlette.routing import Route
+from starlette.routing import Route, Host, Router
 import json
-
 async def router(request: Request):
     path = request.path_params["path"]
-    return Response(path)
-
-app = Starlette(debug=True, routes=[
+    host = request.path_params["host"]
+    return Response(f"path: {path} \nHost: {host}")
+routes=[
     Route("/{path:path}", router)
-    ])
+    ]
+routes = Router(routes)
+routes = [
+    Host('{host}', routes, name="site_api")
+]
+app = Starlette(debug=True, routes=routes)
+
