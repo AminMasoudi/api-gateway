d1db961bae1eb736f7ff1ffa36f9390f42ee8d35
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-25 17:58:06 +0330

    IMP => server

--- server.py
+++ server.py
@@ -2,37 +2,40 @@ from starlette.applications import Starlette
 from starlette.requests import Request
 from starlette.responses import Response
 from starlette.routing import Route, Host, Router
+from utils import settings as config
+import httpx, requests
 import json
-import uvicorn
 
-with open("config.json") as file:
-    conf:dict[str, list[dict[str, str]]] = json.loads(file.read())
-    for k,v in conf.items():
-        conf[k].sort(key=lambda x: x["path"], reverse=True)
+routing_hosts = config.routing_hosts
+
+
 
 async def router(request: Request):
-    path: str = "/"+request.path_params["path"]
+
+    path: str = "/" + request.path_params["path"]
     host = request.path_params["host"]
-    result = conf
+    result = ""
 
-    for route in conf.get(host):
+    for route in routing_hosts.get(host):
         if path.startswith(route["path"]):
-            result = f"Redirected to {route["service"]}"
+            result = "http://"+ route["service"] + path
             break
+    try:
+        response = httpx.request(
+            method=request.method,
+            url=result,
+            # headers=request.headers
+            )
+    except Exception as e:
+        print(e)
+    
+    return Response(
+        content=response.content,
+        status_code=response.status_code,
+        )
 
-    return Response(f"path: {path} \nHost: {host}\nresult: {result}")
 
-
-routes=[
-    Route("/{path:path}", router)
-    ]
+routes = [Route("/{path:path}", router, methods=["POST", "GET"])]
 routes = Router(routes)
-routes = [
-    Host('{host}', routes, name="site_api")
-]
+routes = [Host("{host}", routes, name="site_api")]
 app = Starlette(debug=True, routes=routes)
-
-if __name__ == "__main__":
-    print(conf)
-    uvicorn.run("server:app", host="127.0.0.1", port=8000, log_level="info")
-
