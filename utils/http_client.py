a005e57870eabd571b8bc23612b3ea98884143b0
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-26 10:51:53 +0330

    IMP => Abstracting http clients

---
+++ utils/http_client.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+import httpx
+
+
+class AbstractClient(ABC):
+    def send_request(
+            self,
+            service: str,
+            path: str,
+            protocol:str
+            ):
+        ...
+
+class HttpxClient(AbstractClient):
+
+    async def send_request(
+            self,
+            service,
+            path,
+            protocol:str="http",
+            follow_redirects = True,
+            **data
+            ):
+            
+        url = protocol + "://" + service + path
+
+        try:
+            async with httpx.AsyncClient() as Client:
+                response = await Client.request(
+                    **data,
+                    follow_redirects=follow_redirects,
+                    url=url,
+                    )
+            content, code = response.content, response.status_code
+
+        except httpx.ConnectError:
+            code, content = httpx.codes.SERVICE_UNAVAILABLE
+        except Exception as e:
+            code, content = 500, "Oops!! Something went wrong."
+
+        return (content, code)
