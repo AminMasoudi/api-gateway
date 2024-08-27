f8413d577491225491f9d3a79446dc772e221910
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-27 10:42:42 +0330

    BLACK => formater

--- main.py
+++ main.py
@@ -2,18 +2,19 @@
 import os
 import uvicorn
 
+
 def main():
-    os.environ.setdefault("SETTINGS","settings.dev")
+    os.environ.setdefault("SETTINGS", "settings.dev")
     from utils import settings
-    uvicorn.run(
-        app="server:app",
-        port=settings.PORT,
-        reload=True
-    )
+
+    uvicorn.run(app="server:app", port=settings.PORT, reload=True)
+
+
+
 if __name__ == "__main__":
     # [ ] load_config
     # [ ] preprocessing config files
-    #   [ ] sorts and stuff like that 
+    #   [ ] sorts and stuff like that
     # [ ] setup logger
     # [ ] start listening
     main()
