2d046550c1cf104f80d76f5106ab515d2323eee4
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-27 15:39:52 +0330

    IMP => Test :)

--- test/test_routing_logic.py
+++ test/test_routing_logic.py
@@ -1,3 +1,20 @@
+from utils import settings
+import pytest
 
 
-def test_routing():...
+def test_setting_url_maps_is_sorted():
+
+    maps = settings.url_maps
+    for host, paths in maps.items():
+        for i in range(len(paths) - 1):
+            assert paths[i]["path"] > paths[i+1]["path"]
+
+
+
+
+class TestRouting:
+    def test_find_service_by_path(self):
+        ...    
+
+    def test_find_service_by_host(self):
+        ...
