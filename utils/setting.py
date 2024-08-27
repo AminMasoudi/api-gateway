1a9b283356ee05e731a6071a25406634d06f138c
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-08-27 15:41:35 +0330

    IMP => typing and play around

--- utils/setting.py
+++ utils/setting.py
@@ -1,9 +1,12 @@
 import os
 import importlib
 from functools import lru_cache
+from typing import NewType
 
 from utils.http_client import AbstractClient, HttpxClient
 from model.database import get_db
+
+UrlMaps = NewType("UrlMaps", dict[str, list[dict[str, str]]])
 class Settings:
     def __init__(self) -> None:
         settings_module = os.environ.get("SETTINGS")
@@ -20,14 +23,14 @@ class Settings:
 
     def preprocessing(self):
         # preprocessing on URL_MAPS
-        url_maps:dict[str : list[dict[str, str]]] = getattr(self, "URL_MAPS")
+        url_maps:UrlMaps = getattr(self, "URL_MAPS")
         for k, v in url_maps.items():
             url_maps[k].sort(key=lambda x: x["path"], reverse=True)
         self.__url_maps = url_maps
 
     @property
     @lru_cache
-    def url_maps(self) -> dict:
+    def url_maps(self) -> UrlMaps:
         return self.__url_maps
 
     @property
